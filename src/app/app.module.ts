ff7e7108161408acd4c0a659fd2b27ce9579d924
Ritesh Vishwakarma <Ritesh@cgsadmins-iMac-2.local>
2017-11-02 16:47:34 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { ClientRoutingModule } from './app-routing.module';
 import { ChatComponent } from './chat/chat.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
