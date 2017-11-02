367e2ebcf767e06089c2bd0dac3675e2a4adffa4
Ritesh Vishwakarma <Ritesh@cgsadmins-iMac-2.local>
2017-11-02 19:23:31 +0530

    scss done

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css']
+  styleUrls: ['./loader.component.2.scss']
 })
 export class LoaderComponent implements OnInit {
 
